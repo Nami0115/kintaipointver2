7c510d3c94a42b58ae3b87d4dabe9652d4288ab7
Nami0115 <lifeihave156@gmail.com>
2025-01-09 21:25:13 +0900

    1回目のコミット

---
+++ src/main/java/com/example/kintaipointver2/Kintaipointver2Application.java
@@ -0,0 +1,13 @@
+package com.example.kintaipointver2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kintaipointver2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kintaipointver2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kintaipointver2/Kintaipointver2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kintaipointver2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kintaipointver2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
