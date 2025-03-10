f0e3fbbcd006d63ede99f770923b1a3a3422e482
Nami0115 <lifeihave156@gmail.com>
2025-03-10 14:09:58 +0900

    2025/03/10 14:09 のコミット

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+--roles
+INSERT IGNORE INTO roles (id, name) VALUES (1, 'ROLE_GENERAL');
+INSERT IGNORE INTO roles (id, name) VALUES (2, 'ROLE_ADMIN');
+
+--companies
+INSERT IGNORE INTO companies (id, name) VALUES (1, "株式会社勤怠");
+INSERT IGNORE INTO companies (id, name) VALUES (2, "株式会社ポ・イント");
+INSERT IGNORE INTO companies (id, name) VALUES (3, "株式会社K&P");
+INSERT IGNORE INTO companies (id, name) VALUES (4, "株式会社アテンダンス");
+
